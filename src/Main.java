cf3f022869b0f9fdc8b69406fe4942d3f3f64cf7
Jeferson Castillo <jefersonmanuelcastillolara@gmail.com>
2025-03-26 15:35:49 -0400

    Cambios a la salida

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String nombre = "Juan";
-        System.out.println(nombre);
+        String apellido = "Castillo";
+        System.out.println(nombre + apellido);
     }
 }
