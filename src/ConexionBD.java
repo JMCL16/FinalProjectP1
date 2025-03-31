018cae4b32196f39fb73db202563ee4a6dbdc697
Jeferson Castillo <jefersonmanuelcastillolara@gmail.com>
2025-03-31 14:02:03 -0400

    Documentando codigo

--- src/ConexionBD.java
+++ src/ConexionBD.java
@@ -6,8 +6,9 @@ public class ConexionBD {
     private static ConexionBD instance;
     Connection con;
 
+    //Constructor privado
     private ConexionBD(){
-        //Constructor privado
+        //Conectando la base de datos al proyecto
         try {
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Tarea4", "root", "Jefer1605@");
             System.out.println("Se ha conectado a la BD");
@@ -16,13 +17,16 @@ public class ConexionBD {
         }
     }
 
+    //Patron Singleton para utilizar una misma instancia
     public static ConexionBD getInstance() {
+        //Verificando si la instancia esta creada o no
         if (instance == null){
             instance = new ConexionBD();
         }
         return instance;
     }
 
+    //Metodo para utilizar la conexion de la base de datos
     public Connection getConnection(){
         return con;
     }
